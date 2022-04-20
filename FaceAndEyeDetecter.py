0fcf15e175caa5accab860512977730cdf96b5c2
Miher H. Jivalagian <47829735+miherjivalagian@users.noreply.github.com>
2022-04-20 12:34:55 -0700

    Update FaceAndEyeDetecter.py

--- FaceAndEyeDetecter.py
+++ FaceAndEyeDetecter.py
@@ -8,16 +8,24 @@ eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml'
 
 while True:
     ret,frame = cap.read()
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     faces = faceCascade.detectMultiScale(gray, 1.3,5)
+
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5)
+
         roi_gray = gray[y:y+w,x:x+w]
         roi_color = frame[y:y+h,x:x+w]
+
         eyes= eyeCascade.detectMultiScale(roi_gray,1.3,5)
+
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),5)
+
     cv2.imshow('frame',frame)
+
     if(cv2.waitKey(1)==ord('q')):
         break
 
